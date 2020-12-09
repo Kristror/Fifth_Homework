f99cb7529bd06407f28c6c3f8d47f1153a96c6d8
Kristror <daniil.bezy@ya.ru>
2020-12-09 13:44:09 +0500

    Added CorrLog

---
+++ CorrLog/CheckLogin.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CorrLog
+{
+    partial class Program
+    {
+        static class CheckLogin
+        {
+            public static bool RegCheck(string login)
+            {
+                Regex reg = new Regex("^[A-Za-z][A-Za-z0-9]{2,10}$");
+
+                return reg.IsMatch(login);
+            }
+            public static bool Check(string login)
+            {                
+                if (char.IsDigit(login[0])) return false;
+                if((login.Length < 2)||(login.Length > 10)) return false;
+                for (int i = 0; i < login.Length; i++)
+                {
+                    if ((!char.IsLetter(login[i])) && (!char.IsDigit(login[i]))) return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ CorrLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrLog
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            /// 
+            /// Домашняя работа Безукладникова Даниила
+            /// Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
+            /// содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+            /// а) без использования регулярных выражений;
+            /// б) с использованием регулярных выражений.
+            ///
+            Console.WriteLine("Программа для проверки логина");
+            Console.WriteLine("Введите логин, он должен быть от 2 до 10 символов, содержать только буквы латинского алфавита или цифры, и цифра не может быть первой:");
+            string login = Console.ReadLine();
+            //без использования регулярных выражений
+            if (CheckLogin.Check(login)) Console.WriteLine("Введенный логин соответсвует условиям");
+            else Console.WriteLine("Введенный не логин соответсвует условиям");
+            /// с использованием регулярных выражений.
+            if (CheckLogin.RegCheck(login)) Console.WriteLine("Введенный логин соответсвует условиям");
+            else Console.WriteLine("Введенный не логин соответсвует условиям");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
